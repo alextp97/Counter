bac8483b285a9c8d17a8d611183c9c8b88a6c1d7
Alejandro Torres <alejandro.torres@bosonit.com>
2022-05-09 12:35:31 +0200

    He añadido comentatios en la parte de JavaScript

--- scripts/myscript.js
+++ scripts/myscript.js
@@ -27,15 +27,18 @@ let contador;
 //Código del método para cuando pulse el botón de Start
 start.addEventListener('click', () => {
 
-    console.log(start.value);
+    //Con la función setInterval establezco a 1000 ms que es lo mismo que 1 segundo
     contador = setInterval(() => {
 
+        //Si el botón de contar hacia delante está activo, el contador empieza a sumar los segundos
         if(uCountUp) {
             count += parseInt(step.value);
         }
+        //Sino, si el botón de contar hacia atrás es el que está activo, se empieza a ejecutar este código
         else if (uCountDown) {
             count -= parseInt(step.value);
 
+            //Si el contador llega a ser un número menor que 0, el contador no muestra los números negativos
             if (count < 0) {
                 
                 count = 0;
@@ -45,14 +48,17 @@ start.addEventListener('click', () => {
             count = 0;
         }
 
+        //Igualo num al resultado del count para mostrarlo en la caja de texto
         num.value = count;
         
     }, 1000);
 
 });
 
+//Cuando el usuario pulse el botón de pause, se ejecutará este método
 pause.addEventListener('click', () => {
 
+    //Si el botón es seleccionado, se pone pausado en true y se detiene el intervalo y el contador se detiene
     if (pausado = true) {
         clearInterval(contador)
     }
@@ -61,39 +67,47 @@ pause.addEventListener('click', () => {
     }
 });
 
+//Código para cuando el usuario hace click en el botón de resetear
 reset.addEventListener('click', () => {
 
+    //El valor de la variable count pasa a ser 0 y se le asigna a num.value para que así se le muestre al usuario 0 en el contador
     count = 0;
     num.value = count;
-})
+});
 
 
+//Código para el evento de contar hacia delante
 countUp.addEventListener('click', () => {
-    //console.log('+1')
-    if (uCountUp) {
-        uCountUp = false;
+    
+    //Si el botón de contar hacia delante está en false se pone true el botón de contar hacia atrás
+    if (uCountUp = false) {
+        
         uCountDown = true;
     } else {
         uCountUp = true;
         uCountDown = false;
     }
-})
+});
 
+//Código para el evento de contar hacia delante
 countDown.addEventListener('click', () => {
-    if (uCountDown) {
-        uCountDown = false;
+
+    //Si el botón de contar hacia atrás está false, entonces el botón de contar hacia delante se pone true
+    if (uCountDown = false) {
         uCountUp = true;
     } else {
         uCountDown = true;
         uCountUp = false;
     }
-})
+});
 
+//Código del evento para el botón de donde quiero que empieze a contar 
 setButton.addEventListener('click', () => {
     
+    //Convierto la string de la caja de texto a número para que se muestre y sume o reste segundos correctamente
     count = parseInt(setTo.value);
-    num.value = count;
-    setTo.value = '';
-})
+    num.value = count; //Le paso a num el número almacenado y convertido en count
+
+});
 
 }
